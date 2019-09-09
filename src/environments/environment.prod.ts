047305c465788f5c645e9aa370e5841f014ab7e5
bermejolautaro <tarolau97@hotmail.com>
2019-09-09 00:50:29 -0300

    build done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAGkuRB-l-RkHP5sViS0WvXirYKG9Byw7Y",
+    authDomain: "budgetapp-9ffee.firebaseapp.com",
+    databaseURL: "https://budgetapp-9ffee.firebaseio.com",
+    projectId: "budgetapp-9ffee",
+    storageBucket: "budgetapp-9ffee.appspot.com",
+    messagingSenderId: "387841644361",
+    appId: "1:387841644361:web:259e9890f06749ae"
+  }
 };
